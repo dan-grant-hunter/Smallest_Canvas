27fb6a0f81507f60e5b2d271f3e5aa8a8f3c6a5f
Dan Grant-Hunter <dan.granthunter@gmail.com>
2020-07-30 22:23:14 +0200

    Added initial function framework

--- Smallest_Canvas.py
+++ Smallest_Canvas.py
@@ -1,3 +1,41 @@
 # -----------------------------------------------------------------------------
 # Smallest Canvas
 # -----------------------------------------------------------------------------
+
+# Get Input for Number of Coordinates
+def NumOfCoords():
+    coordsNum = []
+
+
+
+
+# Get Input for all Coordinates
+
+
+
+# Calculate the Smallest Canvas Size
+
+
+
+# Return the Smallest Canvas Size
+
+
+
+# Main Loop
+def mainLoop():
+    pass
+
+
+# Replay Prompt
+if __name__ == '__main__':
+    while True:
+        mainLoop()
+        while True:
+            answer = input('Calculate Again? (Y/N): ')
+            if answer.upper() in ('Y', 'N'):
+                break
+            print('\nInvalid input\n')
+        if answer.upper() == 'Y':
+            continue
+        else:
+            break
